a14b675d0b1b25c464e4dc6c4dc074158216c1fe
Parker Franzi <Parker.franzi@gmail.com>
2019-11-08 15:28:59 -0800

    updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const tastediveAPI = '339797-FindNewI-4PDNXAAE';
 const newsAPI = '3529a7c1a7d54e3587f38460b8114c9a';
-const youtubeAPI = 'AIzaSyA4b9Rx0m_NZPQEbVMa31ZIzbsieueLXhE';
+const youtubeAPI = 'AIzaSyDe0E0oBb5aRvyS9dHlWw4nE653uFgAVS0';
 
 'use strict';
 
